abf81131acf060e2bf988d2fd018609104fc7677
Not Karol <notkarol@gmail.com>
2015-03-21 18:24:54 -0400

    index db-inspired

--- web/index.php
+++ web/index.php
@@ -7,60 +7,29 @@
 <meta name="apple-mobile-web-app-title" content="UFeedMe">
 
 <section class="event-container">
-  <article class="event">
-      <section class="col-1">
-        <img src="img/food_icons/apple.png" alt="food" />
-      </section>
-      <section class="col-2">
-    	<h2>Salsa Dancing</h2>
-    	<ul>
-    		<li>Come dance and eat chips and salsa with UVM's Sass Team!</li>
-    		<li>Tuesday, Nov 1 • 4:00 PM</li>
-    		<li>Patrick Gym</li>
-    	</ul>
-    </section>
-    <section class="col-3">
-      <a href="index.php?title=hi&start_date=3%2F12%2F2015&end_date=4%2F12%2F2015&start_time=3%3A00&end_time=4%3A00&food=hotdogs&location=burlington">★</a>
-        <span class="count">7</span>
-    </section>
-  </article>
-
-    <article class="event">
-      <section class="col-1">
-        <img src="img/food_icons/fries.png" alt="food" />
-      </section>
-      <section class="col-2">
-      <h2>Salsa Dancing</h2>
-      <ul>
-        <li>Come dance and eat chips and salsa with UVM's Sass Team!</li>
-        <li>Tuesday, Nov 1 • 4:00 PM</li>
-        <li>Patrick Gym</li>
-      </ul>
-    </section>
-    <section class="col-3">
-      <a href="#">★</a>
-        <span class="count">27</span>
-    </section>
-  </article>
-
-      <article class="event">
-      <section class="col-1">
-        <img src="img/food_icons/fries.png" alt="food" />
-      </section>
-      <section class="col-2">
-      <h2>Salsa Dancing</h2>
-      <ul>
-        <li>Come dance and eat chips and salsa with UVM's Sass Team!</li>
-        <li>Tuesday, Nov 1 • 4:00 PM</li>
-        <li>Patrick Gym</li>
-      </ul>
-    </section>
-    <section class="col-3">
-      <a href="#">★</a>
-        <span class="count">27</span>
-    </section>
-  </article>
-
+<?php
+$events = get_soon($DB_CONN, 7);
+foreach ($events as $event)
+{
+  echo ' <article class="event">' . "\n";
+  echo ' <section class="col-1">' . "\n";
+  echo ' <img src="img/food_icons/' . $event['picture']. '" alt="food" />' . "\n";
+  echo ' </section>' . "\n";
+  echo ' <section class="col-2">' . "\n";
+  echo '<h2>' . $event['name'] . '</h2>' . "\n";
+  echo '<ul>' . "\n";
+  echo '<li>' . $event['food'] . '</li>' . "\n"; 
+  echo '<li>' . $event['start_time'] . '</li>' . "\n"; 
+  echo '<li>' . $event['location'] . '</li>' . "\n"; 
+  echo '</ul>' . "\n";
+  echo '</section>' . "\n";
+  echo '<section class="col-3">' . "\n";
+  echo '<a href="index.php?title=' . $event['name'] . '&start_time=' . $event['start_time'] . '&end_time=' . $event['end_time'] . '&food=' . $event['food'] . '&location=' . $event['location'] . ' ">★</a>' . "\n";
+  echo '<span class="count">' . strlen($event['name']) . '</span>' . "\n";
+  echo '</section>' . "\n";
+  echo '</article>' . "\n";
+}
+?>
 </section>
 
 <?php include("footer.php") ?>
