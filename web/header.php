bf67344f3ea0c5c957b0a5a8bf32fb57eee92124
Julianna Roen <jsroen@gmail.com>
2015-03-21 09:53:14 -0400

    Added basic files

---
+++ web/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+
+ <title>UFeedMe</title>
+ <meta name="description" content="UFeedMe: connecting students at UVM through events hosting free food">
+ <meta name="author" content="Julianna Roen">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <link rel="stylesheet" href="css/style.css">
+<link rel="shortcut icon" href="UFeedMe_icon.png" >
+
+ <!--[if lt IE 9]>
+ <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->
+
+</head>
---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php include("header.php") ?>
+
+<body>
+  <script src="js/scripts.js"></script>
+
+  <article>
+  	<img class="eimage" src="http://www.vetprofessionals.com/catprofessional/images/home-cat.jpg" alt="event" />
+  		<section>
+  			<span>Description/Food</span>
+  			<span>Time and Location</span>
+  		</section>
+  </article>
+
+<?php include("footer.php") ?>
+</body>
+</html>
