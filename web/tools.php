2e2c69f87b787583bbc50b289ca7ec702b79d70e
Benjamin Marcus Carroll <bcarrol5@silk3.uvm.edu>
2015-03-21 14:17:38 -0400

    updated tools.php

--- web/tools.php
+++ web/tools.php
@@ -83,19 +83,24 @@ function add_user($DB_CONN)
     }
 }
 
-function add_event($DB_CONN, $name, $food, $location, $map_location, $start_time, $duration)
+function add_event($DB_CONN, $name, $food, $location, $map_location, $start_time, $end_time, $picture)
 {
   // Block people
   if (!isset($_SESSION["CAN_POST"]) || $_SESSION["CAN_POST"] == 0)
     {
       return False;
     }
-
-  $sql = "INSERT INTO 'events' ('name','food','location','map_location', 'created_on','duration') VALUES (\"$name\", \"$food\", \"$location\", \"$map_location\", \"$start_time\", \"$duration\");";
+  $netid = $_SESSION['NETID'];
+  $sql = "INSERT INTO `events` (`netid`, `name`,`food`,`location`,`map_location`,`start_time`, `end_time`, `picture`, `created_on`) VALUES (\"$netid\", \"$name\", \"$food\", \"$location\", \"$map_location\", \"$start_time\", \"$end_time\", \"$picture\", NOW());";
   if ($insert_event_result = $DB_CONN->query($sql))
     {
+      echo "SUCCESS!";
       return $mysqli->insert_id;
     }
+  else
+  {
+      echo $sql;
+  }
   return False;
 }
 
