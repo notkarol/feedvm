e98a1b10c8d7e0bfb23f18852156456f886df5f4
Not Karol <notkarol@gmail.com>
2015-03-21 16:56:10 -0400

    map should work, need to finish tools

--- web/map.php
+++ web/map.php
@@ -1,31 +1,86 @@
-<?php 
-include("tools.php");
-include("header.php") ?>
+<?php
+  require_once("tools.php");
+  
+  $events = get_soon($DB_CONN, 7);
 
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+  $icon_names = array("red-blank.png", "blu-blank.png", "grn-blank.png");
+
+  $icon_js = "";
+  foreach ($events as $event)
+  {
+    $event_id = $event[0];
+    $netid = $event[1];
+    $location = $event[2];
+    $start_time = $event[3];
+    $end_time = $event[4];
+    $name = $event[5];
+    $food = $event[6];
+    $created_on = $event[7];
+    $is_fake = $event[8];
+    $map_location = $event[9];
+    $picture = $event[10];
+    $days = 5;
+    $icon = $days < 1 ? $icon_names[0] : $days < 3 ? $icon_names[1] : $icon_names[2];
+    $icon_js .= "
+    geocoder.geocode( { 'address': '$location'}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: map,
+          title: '$name',
+	  icon: iconBase + '$icon'
+	   });
+        var infowindow = new google.maps.InfoWindow({ content: '$name ($location $start_time - $end_time): $food'});
+        google.maps.event.addListener(marker, 'click', function() {
+           infowindow.open(map, marker);
+        });
+       } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+       }
+    });
+
+  
+";
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Simple markers</title>
+    <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
     <script>
+
+function location_of_address(address) {
+}
+
 function initialize() {
-  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  geocoder = new google.maps.Geocoder();
   var mapOptions = {
-    zoom: 4,
-    center: myLatlng
+    zoom: 15,
+    center: new google.maps.LatLng(44.4725, -73.1936)
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
 
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
-  });
+  <?php echo $icon_js; ?>
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
-
-
-<div id="map-canvas"></div>
-
-<?php include("footer.php") ?>
-</body>
+  </head>
+  <body>
+    <div id="map-canvas"></div>
+  </body>
 </html>
+
