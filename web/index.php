1034b48c0ff6d3d6ae7225a66fc7b2e114d3b875
Julianna Roen <jsroen@gmail.com>
2015-03-21 15:22:13 -0400

    changed buttons to input

--- web/index.php
+++ web/index.php
@@ -1,47 +1,56 @@
 <?php include("header.php") ?>
 
   <article class="event">
-  	<article style="background-image: url(http://cdn.playbuzz.com/cdn/213b22c3-5a11-48a4-8a1e-08d7331f30a1/a388cc37-d3e8-4f18-8e18-aea55e826ede.jpg);" class="eimage"></article>
-	<h2>Salsa Dancing</h2>
-	<ul>
-		<li>Come dance and eat chips and salsa with UVM's Sass Team!</li>
-		<li>Tuesday, Nov 1 • 4:00 PM</li>
-		<li>Patrick Gym</li>
-	</ul>
-	<button type="button" onClick="parent.location='index.php?title=hi&time'" >★</button>
+      <img src="img/food_icons/apple.png" alt="food" />
+    	<h2>Salsa Dancing</h2>
+    	<ul>
+    		<li>Come dance and eat chips and salsa with UVM's Sass Team!</li>
+    		<li>Tuesday, Nov 1 • 4:00 PM</li>
+    		<li>Patrick Gym</li>
+    	</ul>
+      <form action="index.php?title=hi&time">
+          <input type="submit" value="★" >
+      </form>
   </article>
 
   <article class="event">
-  	<article style="background-image: url(http://www.extension.org/sites/default/files/w/7/7c/GoatGenemaster2.jpg);" class="eimage"></article>
+    <img src="img/food_icons/apple.png" alt="food" />
   		<h2>ALANA Free Breakfast</h2>
   		<ul>
   			<li>ALANA Free Breakfast</li>
   			<li>Eggs and ham</li>
   			<li>Tuesday, Nov 1 • ALANA Center</li>
   		</ul>
-  		<button type="button">★</button>
+  		<form action="index.php?title=hi&time">
+          <input type="submit" value="★" >
+      </form>
+      <span>27</span>
   </article>
 
   <article class="event">
-  	<article style="background-image: url(http://dewitt.sanford.duke.edu/wp-content/uploads/2014/05/goat.jpg);" class="eimage"></article>
+  	<img src="img/food_icons/apple.png" alt="food" />
   		<h2>Salsa Dancing</h2>
   		<ul>
   			<li>Come dance with UVM's Sass Team!</li>
   			<li>Chips and salsa</li>
   			<li>Tuesday, Nov 1 • Patrick Gym</li>
   		</ul>
-  		<button type="button">★</button>
+  		<form action="index.php?title=hi&time">
+          <input type="submit" value="★" >
+      </form>
   </article>
 
     <article class="event">
-  	<article style="background-image: url(http://dewitt.sanford.duke.edu/wp-content/uploads/2014/05/goat.jpg);" class="eimage"></article>
+  	<img src="img/food_icons/apple.png" alt="food" />
   		<h2>Salsa Dancing</h2>
   		<ul>
   			<li>Come dance with UVM's Sass Team!</li>
   			<li>Chips and salsa</li>
   			<li>Tuesday, Nov 1 • Patrick Gym</li>
   		</ul>
-  		<button type="button">★</button>
+  		<form action="index.php?title=hi&time">
+          <input type="submit" value="★" >
+      </form>
   </article>
 
 <?php include("footer.php") ?>
