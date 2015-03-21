0b500e26350a78d21e80c73a7be781120cfe8fc0
Julianna Roen <jsroen@gmail.com>
2015-03-21 12:50:55 -0400

    Navigation bar done

--- web/footer.php
+++ web/footer.php
@@ -3,3 +3,15 @@
 <p>Footer content</p>
 
 </footer>
+
+		</section>
+		<nav class="shifter-navigation">
+			<ul>
+				<li>Home</li>
+				<li>My Events</li>
+				<li>Map</li>
+				<li>Submit</li>
+			</ul>
+		</nav>
+	</body>
+</html>
