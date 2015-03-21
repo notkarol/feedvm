caf4621c050b8433a6854ea81201286650db51f3
Mark I <mark.ibhm@gmail.com>
2015-03-21 15:06:11 -0400

    hardcoded myevents page

--- web/my_events.php
+++ web/my_events.php
@@ -2,6 +2,18 @@
 
   My events
 
+
+  <article class="event">
+  	<article style="background-image: url(http://dewitt.sanford.duke.edu/wp-content/uploads/2014/05/goat.jpg);" class="eimage"></article>
+  		<h2>Salsa Dancing</h2>
+  		<ul>
+  			<li>Come dance with UVM's Sass Team!</li>
+  			<li>Chips and salsa</li>
+  			<li>Tuesday, Nov 1 • Patrick Gym</li>
+  		</ul>
+  		<button type="button">★</button>
+  </article>
+
 <?php include("footer.php") ?>
 </body>
 </html>
